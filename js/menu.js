49151cb9ef70a02c62403664721bb7e619be2033
PERRIN Olivier <perrinolivier88@gmail.com>
2016-05-10 10:58:17 +0200

    ajout bootstrap

---
+++ js/menu.js
@@ -0,0 +1,44 @@
+function rouge()
+{
+document.getElementById("gauche").style.backgroundColor = "#FF626F";
+var divs = document.querySelectorAll("#date1, #date2, #date3, #date4");
+[].forEach.call(divs, function(div) {
+  div.style.color = "#FF626F";
+});
+}
+
+function vert()
+{
+document.getElementById("gauche").style.backgroundColor = "#5EC3A1";
+var divs = document.querySelectorAll("#date1, #date2, #date3, #date4");
+[].forEach.call(divs, function(div) {
+  div.style.color = "#5EC3A1";
+});
+}
+
+function violet()
+{
+document.getElementById("gauche").style.backgroundColor = "#985498";
+var divs = document.querySelectorAll("#date1, #date2, #date3, #date4");
+[].forEach.call(divs, function(div) {
+  div.style.color = "#985498";
+});
+}
+
+function orange()
+{
+document.getElementById("gauche").style.backgroundColor = "#e67e22";
+var divs = document.querySelectorAll("#date1, #date2, #date3, #date4");
+[].forEach.call(divs, function(div) {
+  div.style.color = "#e67e22";
+});
+}
+
+function bleu()
+{
+document.getElementById("gauche").style.backgroundColor = "#5da4d9";
+var divs = document.querySelectorAll("#date1, #date2, #date3, #date4");
+[].forEach.call(divs, function(div) {
+  div.style.color = "#5da4d9";
+});
+}
